f45c737fd242efff777870f40afe6dd8bd8fb71e
Mehmet ÖCAL <ocalmehmet.2347@gmail.com>
2023-04-07 00:59:26 +0300

    Comments added on Main class

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,19 @@ import javax.mail.MessagingException;
 public class Main {
     public static void main(String[] args) {
 
+        /*
+            1. Terminalden java -version ile versiyon kontrolü yapılmalı. Ben java 17 kullanıyorum
+                java -version
+                java version "17.0.5" 2022-10-18 LTS
+
+            2. Aşağıda mail atılacak adres ve MailIslemleri classında bulunan mail ve password bilgileri güncellenmeli
+
+            3. JarFilesZip isimli klasor altına projeye import edilmesi gereken jar dosyaları yüklendi buradan indirip
+                Libraries altına import edilmeli
+
+            Şu an bu değişiklikler yapılarak mail gönderme işlemi gerçekleştirilebilmektedir.
+         */
+
         MailIslemleri mail = new MailIslemleri();
         try {
             mail.mailGonder("xxxxx@gmail.com");
